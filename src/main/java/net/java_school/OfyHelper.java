687d84eda5298fc67d7befcf8faeb37f3eb51c60
Jonghoon Kim <kim0070@gmail.com>
2019-11-14 01:26:07 +0900

    pom.xml web.xml OfyHelper.java updated

--- src/main/java/net/java_school/OfyHelper.java
+++ src/main/java/net/java_school/OfyHelper.java
@@ -2,7 +2,6 @@ package net.java_school;
 
 import com.googlecode.objectify.ObjectifyService;
 
-import javax.servlet.annotation.WebListener;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextListener;
 import javax.servlet.ServletContextEvent;
@@ -15,23 +14,22 @@ import net.java_school.board.Comment;
  * OfyHelper, a ServletContextListener, is setup in web.xml to run before a JSP is run.  This is
  * required to let JSP's access Ofy.
  **/
-@WebListener
 public class OfyHelper implements ServletContextListener {
-  public static void register() {
-    ObjectifyService.register(Article.class);
-    ObjectifyService.register(AttachFile.class);
-    ObjectifyService.register(Comment.class);
-  }
+	public static void register() {
+		ObjectifyService.register(Article.class);
+		ObjectifyService.register(AttachFile.class);
+		ObjectifyService.register(Comment.class);
+	}
 
-  public void contextInitialized(ServletContextEvent event) {
-    ServletContext sc = event.getServletContext();
-    sc.log("OfyHelper: Init");
-    // This will be invoked as part of a warmup request, or the first user
-    // request if no warmup request was invoked.
-    register();
-  }
+	public void contextInitialized(ServletContextEvent event) {
+		ServletContext sc = event.getServletContext();
+		sc.log("OfyHelper: Init");
+		// This will be invoked as part of a warmup request, or the first user
+		// request if no warmup request was invoked.
+		register();
+	}
 
-  public void contextDestroyed(ServletContextEvent event) {
-    // App Engine does not currently invoke this method.
-  }
+	public void contextDestroyed(ServletContextEvent event) {
+		// App Engine does not currently invoke this method.
+	}
 }
